cc4bffa1f3c8bb8c672302f42c0feaa3ac2164e5
Kristina <8kristy@gmail.com>
2023-12-09 11:01:07 +0000

    Day 9

---
+++ day9/sol1.py
@@ -0,0 +1,16 @@
+def get_next_number(sequence):
+    if all([x == 0 for x in sequence]):
+        return 0
+    else:
+        pairs = list(zip(sequence, sequence[1:]))
+        diff_sequence = [x[1] - x[0] for x in pairs]
+        return sequence[-1] + get_next_number(diff_sequence)
+    
+sum = 0
+
+with open("input", 'r') as f:
+    for line in f.readlines():
+        sequence = [int(x) for x in line.strip().split(" ")]
+        sum += get_next_number(sequence)
+        
+print(sum)
